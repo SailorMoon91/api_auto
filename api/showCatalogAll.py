e2113c3260d2d426aa6b1e8ac2bc6b15980b1cf1
z781627628@163.com <z781627628@163.com>
2022-05-13 16:28:42 +0800

    20220513-test

--- api/showCatalogAll.py
+++ api/showCatalogAll.py
@@ -2,7 +2,7 @@ import requests
 
 def showCatalogAll(base_url,parentCode, spcode,**kwargs):
     url = base_url + "/catalog/showCatalogAll"
-    body ={
+    body = {
         "spCode": spcode,
         "parentCode": parentCode
     }
@@ -12,5 +12,5 @@ def showCatalogAll(base_url,parentCode, spcode,**kwargs):
 
 if __name__ == "__main__":
     base_url = "http://10.0.1.156:8183/vas-rest"
-    r = showCatalogAll(base_url, parentCode="f5fecedac6e0469d98f475c60084c0d1",spcode="86cd9e3e0fa440b9866d7b89021b8c51",orderByField="sequence")
+    r = showCatalogAll(base_url, parentCode="f5fecedac6e0469d98f475c60084c0d1", spcode="86cd9e3e0fa440b9866d7b89021b8c51",orderByField="sequence")
     print(r.text)
