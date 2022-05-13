25b094eae14e960f7139decd3c8ee87a9ff0e8b3
z781627628@163.com <z781627628@163.com>
2022-05-13 16:42:17 +0800

    20220513-test

--- testcases/test_showCatalogRecommend.py
+++ testcases/test_showCatalogRecommend.py
@@ -1,9 +1,9 @@
-from api.showCatalogAll import showCatalogAll
+from api.showCatalogRecommend import showCatalogRecommend
 import pytest
 
 @pytest.mark.smoke
-def test_showCatalogAll_success(base_url):
-    r = showCatalogAll(base_url, "f5fecedac6e0469d98f475c60084c0d1","86cd9e3e0fa440b9866d7b89021b8c59")
+def test_showCatalogRecommend_success(base_url):
+    r = showCatalogRecommend(base_url, "f5fecedac6e0469d98f475c60084c0d1", "86cd9e3e0fa440b9866d7b89021b8c59")
     print(r.text)
     assert len(r.json()["data"]) == 5
 
@@ -11,8 +11,8 @@ def test_showCatalogAll_success(base_url):
     ["", {"code": 8003, "message":"专区code为空"}],
     ["86cd9e3e0fa440b9866d7b89021b8c51", {"code": 0, "message": "操作成功"}]
 ])
-def test_showCatalogAll_fail(base_url,test_input,expected):
-    r = showCatalogAll(base_url,"f5fecedac6e0469d98f475c60084c0d1",test_input)
+def test_showCatalogRecommend_fail(base_url,test_input,expected):
+    r = showCatalogRecommend(base_url, "f5fecedac6e0469d98f475c60084c0d1", test_input)
     print(r.text)
     assert r.json()["code"] == expected["code"]
     assert r.json()["message"] == expected["message"]
