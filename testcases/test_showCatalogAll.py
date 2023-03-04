aa0699db6e569508cc82108c0cddf4c3a1caaff1
z781627628@163.com <z781627628@163.com>
2023-03-05 03:36:44 +0800

    20220513-test

--- testcases/test_showCatalogAll.py
+++ testcases/test_showCatalogAll.py
@@ -3,16 +3,16 @@ import pytest
 
 @pytest.mark.smoke
 def test_showCatalogAll_success(base_url):
-    r = showCatalogAll(base_url, "f5fecedac6e0469d98f475c60084c0d1","86cd9e3e0fa440b9866d7b89021b8c59")
+    r = showCatalogAll(base_url, "ecb319c70ba445d58ba4e128e041063a","71e1545719c54c468b7ee6033121f551")
     print(r.text)
-    assert len(r.json()["data"]) == 5
+    assert len(r.json()["data"]) == 11
 
 @pytest.mark.parametrize("test_input,expected",[
     ["", {"code": 8003, "message":"专区code为空"}],
-    ["86cd9e3e0fa440b9866d7b89021b8c51", {"code": 0, "message": "操作成功"}]
+    ["71e1545719c54c468b7ee6033121f551", {"code": 0, "message": "操作成功"}]
 ])
 def test_showCatalogAll_fail(base_url, test_input, expected):
-    r = showCatalogAll(base_url, "f5fecedac6e0469d98f475c60084c0d1", test_input)
+    r = showCatalogAll(base_url, "ecb319c70ba445d58ba4e128e041063a", test_input)
     print(r.text)
     assert r.json()["code"] == expected["code"]
     assert r.json()["message"] == expected["message"]
